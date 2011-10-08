743260a244dd1df153b8166a1be2574597c40134
duynp2010@gmail.com <duynp2010@gmail.com@83fd1080-c130-15ca-f1d6-bc077d6ad154>
2011-10-08 17:05:35 +0000

    doi lai source CuteEditor, ban truoc up cho kh toan bi loi

---
+++ trunk/NewsVn/NewsVn.Web/CuteSoft_Client/CuteEditor/Scripts/Dialog/Dialog_Keyboard.js
@@ -0,0 +1,1 @@
+var OxOb403=["keyboard_area","length","value"];var editor=Window_GetDialogArguments(window);function do_insert(){var Ox433=Window_GetElement(window,OxOb403[0],true);if(Ox433[OxOb403[2]][OxOb403[1]]>0){editor.PasteHTML(Ox433.value);} ;} ;function do_Close(){Window_CloseDialog(window);} ;
