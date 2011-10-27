30a2b686bc94f16a4c50cc29885ae71e3b26d67b
koha2924@gmail.com <koha2924@gmail.com@83fd1080-c130-15ca-f1d6-bc077d6ad154>
2011-10-27 09:25:27 +0000

    phần quản lý banner: - Thêm vào 2 Entity. - Chỉnh sửa usercontrol config. - Thêm vào các trang xử lý banner trong phần Account/SiteAdmin/Misc/ - Thêm vào các usercontrol xử lý banner trong Modules. - Kéo BannerControl vào user.master

---
+++ NewsVn/NewsVn.Impl/Entity/BannerDetail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Runtime.Serialization;
+using Vietstream.Data.Model;
+
+namespace NewsVn.Impl.Entity
+{
+    [Table(Name = "BannerDetail")]
+    public class BannerDetail : Base, ISerializable
+    {
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int ID { get; set; }
+
+        [Column]
+        public int BannerID { get; set; }
+
+        [Column]
+        public int Width { get; set; }
+
+        [Column]
+        public int Height { get; set; }
+
+        [Column]
+        public string Title { get; set; }
+
+        [Column]
+        public string Url { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
